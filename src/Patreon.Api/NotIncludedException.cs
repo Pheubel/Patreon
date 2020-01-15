22690cd3062566f5d180395418df20ad904ba659
Pheubel <luca.guagliardo@live.nl>
2020-01-15 12:17:12 +0100

    Created NotIncludedException
    For  preventing acces to properties not included in the request.

---
+++ src/Patreon.Api/NotIncludedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Patreon.Api
+{
+    /// <summary> The error that occurs when an excluded property gets accessed.</summary>
+    /// <typeparam name="T"> The flags used for marking uncluded properies.</typeparam>
+    public class NotIncludedException<T> : Exception
+        where T : struct, Enum
+    {
+        /// <summary> Initializes a new instance of <see cref="NotIncludedException{T}"/> class with flag information.</summary>
+        /// <param name="missingFlag"> The flag requirement that has not been met.</param>
+        public NotIncludedException(T missingFlag) : 
+            base($"Flag {missingFlag} is not included in the object.") { }
+
+        /// <summary> Initializes a new instance of <see cref="NotIncludedException{T}"/> class with flag and property information.</summary>
+        /// <param name="missingFlag"> The flag requirement that has not been met.</param>
+        /// <param name="propertyName"> The property name where the required flag has not been met.</param>
+        public NotIncludedException(T missingFlag, string propertyName) : 
+            base($"{propertyName} requires {missingFlag} to be set in the object") { }
+    }
+}
