2606c06035c646a3181ae8584921f9606ee549fb
Pheubel <luca.guagliardo@live.nl>
2020-01-16 22:48:16 +0100

    Create IAddress.cs

---
+++ src/Patreon.Api/V2/Core/Resources/IAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Patreon.Api.V2.Core.Resources
+{
+    public interface IAddress : IPatreonResource
+    {
+        string? Addressee { get; }
+        string? LineOne { get; }
+        string? LineTwo { get; }
+        string? PostalCode { get; }
+        string City { get; }
+        string? State { get; }
+        string? Country { get; }
+        string? PhoneNumber { get; }
+        DateTime CreatedAt { get; }
+    }
+}
