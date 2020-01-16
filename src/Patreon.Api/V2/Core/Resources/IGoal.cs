53f345625eb8df9c5ebbb51dab438462e11c84b6
Pheubel <luca.guagliardo@live.nl>
2020-01-16 22:54:29 +0100

    Create IGoal.cs

---
+++ src/Patreon.Api/V2/Core/Resources/IGoal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Patreon.Api.V2.Core.Resources
+{
+    public interface IGoal : IPatreonResource
+    {
+        int AmountCents { get; }
+        string Title { get; }
+        string? Description { get; }
+        DateTime CreatedAt { get; }
+        DateTime? ReachedAt { get; }
+        byte CompletedPercentage { get; }
+    }
+}
