a45c4b34b3b08c5c00d727b6c9276b542fe3e6e3
Pheubel <luca.guagliardo@live.nl>
2020-01-16 22:48:23 +0100

    Create IDeliverable.cs

---
+++ src/Patreon.Api/V2/Core/Resources/IDeliverable.cs
@@ -0,0 +1,12 @@
+﻿using Patreon.Api.V2.Resources;
+using System;
+
+namespace Patreon.Api.V2.Core.Resources
+{
+    public interface IDeliverable : IPatreonResource
+    {
+        DateTime CompletedAt { get; }
+        Deliverable.Status DeliveryStatus { get; }
+        DateTime DueAt { get; }
+    }
+}
