38c1243f1051a05966b5025e0edc35cb8afeae1e
Pheubel <luca.guagliardo@live.nl>
2020-01-16 22:49:04 +0100

    Create TokenResponse.cs

---
+++ src/Patreon.Api/V2/TokenResponse.cs
@@ -0,0 +1,19 @@
+﻿#nullable disable
+
+using Patreon.Api.Converters;
+using Patreon.Api.V2.Core;
+using System;
+using System.Text.Json.Serialization;
+
+namespace Patreon.Api.V2
+{
+    [JsonConverter(typeof(TokenResponseConverter))]
+    public class TokenResponse : ITokenResponse<ScopeField>
+    {
+        public string AccessToken { get; internal set; }
+        public string RefreshToken { get; internal set; }
+        public DateTime ExpiresAt { get; internal set; }
+        public ScopeField Scope { get; internal set; }
+        public const string TokenType = "Bearer";
+    }
+}
