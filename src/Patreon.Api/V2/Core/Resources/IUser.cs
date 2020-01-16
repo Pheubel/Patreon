239fc0fde151660635edb03ea05f9bb82b92e99e
Pheubel <luca.guagliardo@live.nl>
2020-01-17 00:12:17 +0100

    Create IUser.cs

---
+++ src/Patreon.Api/V2/Core/Resources/IUser.cs
@@ -0,0 +1,23 @@
+﻿using Patreon.Api.V2.Resources;
+using System;
+
+namespace Patreon.Api.V2.Core.Resources
+{
+    public interface IUser
+    {
+        string Email { get; }
+        string? FirstName { get; }
+        string? LastName { get; }
+        string? Vanity { get; }
+        string? About { get; }
+        string Url { get; }
+        bool IsEmailVerified { get; }
+        bool CanSeeNsfw { get; }
+        bool HidesPledges { get; }
+        DateTime CreatedAt { get; }
+        string AvatarUrl { get; }
+        string AvatarThumbnailUrl { get; }
+        int LikeCount { get; }
+        SocialConnections SocialConnections { get; }
+    }
+}
