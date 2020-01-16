682b6b415d6d39def84000e7368e354e635e52ff
Pheubel <luca.guagliardo@live.nl>
2020-01-16 22:49:33 +0100

    Create IBenefit.cs

---
+++ src/Patreon.Api/V2/Core/Resources/IBenefit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Patreon.Api.V2.Core.Resources
+{
+    public interface IBenefit : IPatreonResource
+    {
+        string Title { get; }
+        string? Description { get; }
+        string? BenefitType {get;}
+        string? RuleType { get; }
+        DateTime CreatedAt { get; }
+        int DeliveredDeliverablesCount { get; }
+        int NotDeliveredDeliverablesCount { get; }
+        DateTime? NextDeliverableDueDate { get; }
+        int TiersCount { get; }
+        bool IsDeleted { get; }
+        bool IsPublished { get; }
+        bool IsEnded { get; }
+        string? AppExternalId { get; }
+        string? AppMeta { get; }
+    }
+}
