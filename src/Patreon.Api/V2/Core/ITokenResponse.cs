6abe90a2c90c9fdf06f0b22072d6b72c15f73fbd
Pheubel <luca.guagliardo@live.nl>
2020-01-16 22:48:33 +0100

    Create ITokenResponse.cs

---
+++ src/Patreon.Api/V2/Core/ITokenResponse.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+
+using System;
+
+namespace Patreon.Api.V2.Core
+{
+    public interface ITokenResponse<TScope>
+        where TScope: struct, Enum
+    {
+        string AccessToken { get; }
+        string RefreshToken { get; }
+        DateTime ExpiresAt { get; }
+        TScope Scope { get; }
+    }
+}
