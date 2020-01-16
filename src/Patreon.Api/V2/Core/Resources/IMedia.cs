b11d17dc056a631d75f1be7c840992f8388541b4
Pheubel <luca.guagliardo@live.nl>
2020-01-17 00:04:24 +0100

    Create IMedia.cs

---
+++ src/Patreon.Api/V2/Core/Resources/IMedia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Patreon.Api.V2.Core.Resources
+{
+    public interface IMedia
+    {
+        string FileName { get; }
+        int SizeBytes { get; }
+        string MimeType { get; }
+        string State { get; }
+        string OwnerType { get; }
+        string OwnerId { get; }
+        string OwnerRelationship { get; }
+        DateTime UploadExpiresAt { get; }
+        string UploadUrl { get; }
+        string UploadParameters { get; }
+        string DownloadUrl { get; }
+        string[] ImageUrls { get; }
+        DateTime CreatedAt { get; }
+        string? MetaData { get; }
+    }
+}
