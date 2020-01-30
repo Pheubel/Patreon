568cfae650b29258e4f25bda49f8b6e81f057647
Pheubel <luca.guagliardo@live.nl>
2020-01-30 11:59:27 +0100

    Update ICampaign.cs

--- src/Patreon.Api/V2/Core/Resources/ICampaign.cs
+++ src/Patreon.Api/V2/Core/Resources/ICampaign.cs
@@ -8,25 +8,32 @@ namespace Patreon.Api.V2.Core.Resources
     {
         string Summary { get; }
         string CreationName { get; }
-        string PayPername { get;  }
-        string OneLiner { get;  }
-        string MainVideoEmbed { get;  }
-        string MainVideoUrl { get;  }
-        string ImageUrl { get;  }
-        string ImageSmallUrl { get;  }
-        string ThanksVideoUrl { get;  }
-        string ThanksEmbed { get;  }
-        string ThanksMessage { get;  }
-        bool IsMonthly { get;  }
-        bool HasRss { get;  }
-        bool HasSentRssNotification { get;  }
-        string RssFeedTitle { get;  }
-        string RssArtworkUrl { get;  }
-        bool IsNsfw { get;  }
-        bool IsChargedImmediately { get;  }
-        DateTime CreatedAt { get;  }
-        DateTime? PublishedAt { get;  }
-        string PledgeUrl { get;  }
+        string PayPername { get; }
+        string OneLiner { get; }
+        string MainVideoEmbed { get; }
+        string MainVideoUrl { get; }
+        string ImageUrl { get; }
+        string ImageSmallUrl { get; }
+        string ThanksVideoUrl { get; }
+        string ThanksEmbed { get; }
+        string ThanksMessage { get; }
+        bool IsMonthly { get; }
+        bool HasRss { get; }
+        bool HasSentRssNotification { get; }
+        string RssFeedTitle { get; }
+        string RssArtworkUrl { get; }
+        bool IsNsfw { get; }
+        bool IsChargedImmediately { get; }
+        DateTime CreatedAt { get; }
+        DateTime? PublishedAt { get; }
+        string PledgeUrl { get; }
+        int PatronCount { get; }
+        ulong? DiscordServerId { get; }
+        string GoogleAnalyticsId { get; }
+        bool ShowEarnings { get; }
+        string Vanity { get; }
+        string Url { get; }
+
         IReadOnlyCollection<Tier> Tiers { get; }
         User Creator { get; }
         IReadOnlyCollection<Benefit> Benefits { get; }
