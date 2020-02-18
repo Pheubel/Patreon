304dee9a959d987460b9041cdf81fef4d61295d5
Pheubel <luca.guagliardo@live.nl>
2020-02-18 15:50:48 +0100

    Create IIdentityBuilder.cs

---
+++ src/Patreon.Api/V2/Core/Builders/IIdentityBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Patreon.Api.V2.Core.Resources;
+using System.Net.Http;
+
+namespace Patreon.Api.V2.Core.Builders
+{
+    public interface IIdentityBuilder<TUser>
+        where TUser : IUser
+    {
+        string ApiEndpoint { get; }
+        FormUrlEncodedContent BuildRequestContent();
+        TUser BuildIdentity(string userString);
+    }
+}
