e4d1fb4a6eed4b35076e0b9d0bd3e1a8d3f18a8b
Pheubel <luca.guagliardo@live.nl>
2020-02-18 15:50:35 +0100

    Create IIdentityClient.cs

---
+++ src/Patreon.Api/V2/Core/IIdentityClient.cs
@@ -0,0 +1,15 @@
+﻿using Patreon.Api.V2.Core.Builders;
+using Patreon.Api.V2.Core.Resources;
+using System;
+using System.Threading.Tasks;
+
+namespace Patreon.Api.V2.Core
+{
+    public interface IIdentityClient<TUser,TToken,TTokenScope>
+        where TUser : IUser
+        where TToken : ITokenResponse<TTokenScope>
+        where TTokenScope : struct, Enum
+    {
+        Task<TUser> GetIdentityAsync(TToken token, IIdentityBuilder<TUser> builder);
+    }
+}
